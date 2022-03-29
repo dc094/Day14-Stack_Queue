83d087ffab1936fd6d0481c88dfa93aebfa8db40
Durgesh094 <durgeshchavan1994@gmail.com>
2022-03-29 15:17:58 +0530

    Perform all operation of stack and queue

---
+++ src/com/bridgelabz/stackqueue/INode.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.stackqueue;
+
+public interface INode<K> {
+    K getKey();
+    void setKey(K key);
+
+    INode getNext();
+    void setNext(INode next);
+}
---
+++ src/com/bridgelabz/stackqueue/Node.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.stackqueue;
+
+public class Node <K> implements INode <K> {
+    private K key;
+    private INode next;
+
+    public Node(K key){
+        this.key = key;
+        this.next = null;
+    }
+
+    @Override
+    public K getKey(){
+        return key;
+    }
+    @Override
+    public void setKey(K key){
+        this.key = key;
+    }
+    @Override
+    public INode <K> getNext(){
+        return next;
+    }
+    @Override
+    public void setNext(INode next){
+        this.next = (Node <K>) next;
+    }
+}
